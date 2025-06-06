baff277768ab7ea3254d640735991df70ca63193
Emilly0000 <emillyc@aluno.ifce.edu.br>
2025-06-06 16:34:10 -0300

    Emilly0000 altera page.tsx com mensagem personalizada

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Olá, esse é meu app React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 25,
+    color: 'white',
   },
 });
